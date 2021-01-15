7683532db327c8acc7f44a46a908c7925614e5de
plazik1239 <plazik1239@users.noreply.github.com>
2021-01-15 18:25:02 +0100

    Added Answer window + Category window + basic game logic

---
+++ Question.py
@@ -0,0 +1,56 @@
+class Question:
+    def __init__(self, prompt, anwser):
+        self.prompt = prompt
+        self.answer = anwser
+
+    def getPrompt(self):
+        return self.prompt
+
+    def getAnswer(self):
+        return self.answer
+
+
+class Questions:
+    prompts_Pokemon = [
+        "W jakiego pokemona evouluje pikachu?\n\nA) Jolteon \n\nB) Pichu \n\nC) Electrive \n\nD) Raichu",
+        "Jaki Poke Ball jest najskuteczniejszy?\n\nA) Great Ball \n\nB) Master Ball \n\nC) Ultra Ball \n\nD) Nest Ball",
+        "Ile trzeba posiadać odznak aby wziąć udział w lidze Pokemon?\n\nA) 6 \n\nB) 7 \n\nC) 8 \n\nD) 9",
+        "Jakie urządzenie pozwala na sprawdzenie informacji o pokemonie?\n\nA) Pokecounter \n\nB) Pokedex \n\n"
+        "C) Pokefinder \n\n""D) Pokeinfo",
+        "Jakie są trzy typy pokemonów startowych?\n\nA) Trawiasty,Ognisty i Wodny \n\nB) Elektryczny,Ziemny i Trujący\n\n"
+        "C)Smoczy,Latający i Normalny \n\nD) Psychiczny,Walczący i Duch",
+        "Jak nazywa się głowny rywal Asha w podróży po regionie Kanto?\n\nA) Lance \n\nB) Giovanni \n\nC) Gary \n\nD) James",
+        "Który z pokemonów nie jest evolucją Evee?\n\nA) Vaporeon \n\nB) Sylveon \n\nC) Leafeon \n\nD) Flygon",
+        "Jak nazywa się pokemon uznawany za Boga?\n\nA) Arceus \n\nB) Mew \n\nC) Jirachi \nD) Celebi",
+        "Jak brzmią imiona dwóch członków zespołu R?\n\nA)Jessie i James \n\nB) Joanie i James\n\n"
+        "C) Jenny i John\n\nD)Jessie i John",
+        "Kto użyczył głosu tytułowemu bohaterowi w filmie Detektyw Pikachu?\n\nA) Justice Smith\n\nB) Omar Chaparro\n\n"
+        "C) Bill Nighy\n\nD) Ryan Reynolds",
+    ]
+
+    questions = [
+        Question(prompts_Pokemon[0], "D"),
+        Question(prompts_Pokemon[1], "B"),
+        Question(prompts_Pokemon[2], "C"),
+        Question(prompts_Pokemon[3], "B"),
+        Question(prompts_Pokemon[4], "A"),
+        Question(prompts_Pokemon[5], "C"),
+        Question(prompts_Pokemon[6], "D"),
+        Question(prompts_Pokemon[7], "A"),
+        Question(prompts_Pokemon[8], "A"),
+        Question(prompts_Pokemon[9], "D"),
+    ]
+
+    def getQuestion(self, index):
+        return self.questions[index]
+
+
+prompts_Games = [
+
+]
+
+prompts_Music = [
+
+]
+
+
--- main.py
+++ main.py
@@ -1,32 +1,105 @@
 import sys
-
-from PyQt5 import QtWidgets
 from PyQt5.QtGui import QMovie
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.uic import loadUi
+from Question import Questions, Question
+
+
+class PlayWindow(QMainWindow):
+
+    def __init__(self, category):
+        super().__init__()
+        loadUi("PlayWindow.ui", self)
+        self.player = Player()
+        self.movie = QMovie("background.gif")
+        self.label.setMovie(self.movie)
+        self.movie.start()
+        self.setFixedHeight(560)
+        self.setFixedWidth(660)
+        self.i = 0  # number of question
+        if category == "all":
+            self.header.setText("All Categories")
+            self.questions = Questions()
+        else:
+            self.header.setText(category)
+            self.questions = Questions()
+        self.question = self.questions.getQuestion(self.i)
+        self.questionLabel.setText(self.question.prompt)
+        self.pushButtonA.clicked.connect(lambda: self.checkAnswer("A"))
+        self.pushButtonB.clicked.connect(lambda: self.checkAnswer("B"))
+        self.pushButtonC.clicked.connect(lambda: self.checkAnswer("C"))
+        self.pushButtonD.clicked.connect(lambda: self.checkAnswer("D"))
+
+    def checkAnswer(self, answer):
+        if answer == self.question.answer:
+            self.player.points += 1
+        self.i += 1
+        print(self.player.points)
+        self.nextQeustion()
+
+    def nextQeustion(self):
+        self.question = self.questions.getQuestion(self.i)
+        self.questionLabel.setText(self.question.prompt)
+        self.labelNrQ.setText("Question " + str(self.i + 1))
+
+
+class CategoryWindow(QMainWindow):
+
+    def __init__(self):
+        super(CategoryWindow, self).__init__()
+        self.dialog = None
+        loadUi("CategoryWindow.ui", self)
+        self.movie = QMovie("background.gif")
+        self.label.setMovie(self.movie)
+        self.movie.start()
+        self.setFixedHeight(560)
+        self.setFixedWidth(660)
+        self.backButton.clicked.connect(self.back)
+        self.Button1.clicked.connect(lambda: self.play("Pokemon"))
+        self.Button2.clicked.connect(lambda: self.play("Games"))
+        self.Button3.clicked.connect(lambda: self.play("Music"))
+
+    def back(self):
+        self.close()
+        main_window.showNormal()
+
+    def play(self, text):
+        self.dialog = PlayWindow(text)
+        self.dialog.show()
+        self.close()
 
 
 class MainWindow(QMainWindow):
+
     def __init__(self):
         super(MainWindow, self).__init__()
+        self.dialog = PlayWindow("all")
+        self.dialog1 = CategoryWindow()
+        self.setFixedHeight(560)
+        self.setFixedWidth(660)
         loadUi("MainWindow.ui", self)
         self.movie = QMovie("background.gif")
         self.label.setMovie(self.movie)
         self.movie.start()
-        self.startButton.setStyleSheet("QPushButton{background: rgba(255,255,255, 150);}")
-        self.categoryButton.setStyleSheet("QPushButton{background: rgba(255,255,255, 150);}")
         self.exitButton.clicked.connect(sys.exit)
+        self.startButton.clicked.connect(self.play)
+        self.categoryButton.clicked.connect(self.category)
+
+    def play(self):
+        self.dialog.show()
+        self.close()
+
+    def category(self):
+        self.dialog1.show()
+        self.close()
 
 
-def window():
-    app = QApplication(sys.argv)
-    main_window = MainWindow()
-    widget = QtWidgets.QStackedWidget()
-    widget.addWidget(main_window)
-    widget.setFixedWidth(660)
-    widget.setFixedHeight(560)
-    widget.show()
-    sys.exit(app.exec())
+class Player:
+    def __init__(self):
+        self.points = 0
 
 
-window()
+app = QApplication(sys.argv)
+main_window = MainWindow()
+main_window.show()
+sys.exit(app.exec())
